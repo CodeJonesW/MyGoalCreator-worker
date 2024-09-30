eb5c4286d6bba8e9e9470aa592c14f164df96d7a
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-09-30 12:05:10 -0500

    add test user to schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS Users;
 CREATE TABLE IF NOT EXISTS Users (UserId INTEGER PRIMARY KEY, email TEXT, transcription_minutes INTEGER, password TEXT);
+INSERT INTO Users (email, transcription_minutes, password) VALUES ('test@test.com', 0, 'password');
