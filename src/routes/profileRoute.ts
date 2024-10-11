6f9af7eddfeaf8a8abd01de5b32c96addc43c363
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-10 22:53:35 -0500

    fix accessing recent goal

--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -20,7 +20,7 @@ export const profileRoute = async (request: Request, env: Env): Promise<Response
 		});
 	}
 
-	return new Response(JSON.stringify({ user: userFromDb, goals: goalsQuery.results, recentGoal: recentGoal ? recentGoal.results : null }), {
+	return new Response(JSON.stringify({ user: userFromDb, goals: goalsQuery.results, recentGoal: recentGoal ? recentGoal : null }), {
 		status: 200,
 		headers: { 'Content-Type': 'application/json' },
 	});
