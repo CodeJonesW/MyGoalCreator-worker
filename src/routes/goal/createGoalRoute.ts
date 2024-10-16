5cd8d44297e2398c8f7af5fb6287cf092d890fa4
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-16 13:41:29 -0500

    refactor ai completion from goal route to external util file

--- src/routes/goal/createGoalRoute.ts
+++ src/routes/goal/createGoalRoute.ts
@@ -2,6 +2,7 @@ import { Env } from '../../types';
 import OpenAI from 'openai';
 import { verifyToken } from '../../utils/auth';
 import { checkIfUserHasAnalyzeRequests } from '../../utils/db_queries';
+import { createGoal } from '../../utils/ai_completions';
 
 export const createGoalRoute = async (request: Request, env: Env): Promise<Response> => {
 	try {
@@ -19,8 +20,6 @@ export const createGoalRoute = async (request: Request, env: Env): Promise<Respo
 		}
 
 		const { goal, prompt: areaOfFocus, timeline }: any = await request.json();
-		const overAllTimeLine = timeline ? timeline : '1 year';
-		const planIncrements = overAllTimeLine === '1 day' ? '' : timeline === '1 week' ? 'daily' : 'weekly';
 
 		if (!goal) {
 			return new Response(JSON.stringify({ error: 'URL is required' }), {
@@ -29,75 +28,7 @@ export const createGoalRoute = async (request: Request, env: Env): Promise<Respo
 			});
 		}
 
-		const completion = await openai.chat.completions.create({
-			stream: true,
-			messages: [
-				{
-					role: 'system',
-					content: `You are an expert in the field of ${goal}`,
-				},
-				{ role: 'user', content: `My goal is to ${goal}. ${areaOfFocus ? `My areas of focus are ${areaOfFocus}` : ''}` },
-				{
-					role: 'system',
-					content: `You are passionate about explaining sub points of the goal in detail`,
-				},
-				{ role: 'system', content: `Outline detailed plan to achieve the goal in ${timeline}.` },
-				{ role: 'system', content: `Expand or contract the plan relative to the length of the goal's timeline.` },
-				{
-					role: 'system',
-					content: `Please format your response in valid Markdown, adhering to the following:
-					- Use headings with "#" for levels (e.g., "#", "##").
-					- Do not use bullet points or numbered lists.
-					Ensure the Markdown is clean and easy to copy into any Markdown editor.`,
-				},
-			],
-			model: 'gpt-4o-mini',
-		});
-
-		let buffer = '';
-		let rawTotalResponse = '';
-
-		const stream = new ReadableStream({
-			async start(controller) {
-				const encoder = new TextEncoder();
-				for await (const chunk of completion) {
-					let content = chunk.choices[0]?.delta?.content;
-
-					if (content) {
-						rawTotalResponse += content;
-						buffer += content;
-
-						let lines = buffer.split(/(?=\n|^#{1,4}|\s-\s|\n\s\*\s|\n\d+\.\s)/);
-						buffer = '';
-
-						lines.forEach((line, index) => {
-							if (index === lines.length - 1 && !line.endsWith('\n')) {
-								buffer = line;
-							} else {
-								controller.enqueue(encoder.encode(line));
-							}
-						});
-					}
-				}
-
-				if (buffer) {
-					controller.enqueue(encoder.encode(buffer));
-				}
-
-				controller.enqueue(encoder.encode(`event: done\n\n`));
-
-				try {
-					await env.DB.prepare(`UPDATE Users SET analyze_requests = analyze_requests - 1 WHERE user_id = ?`).bind(user.user_id).run();
-					await env.DB.prepare(`INSERT INTO Goals (user_id, goal_name, plan, time_line, aof) VALUES (?, ?, ?, ?, ?)`)
-						.bind(user.user_id, goal, rawTotalResponse, overAllTimeLine, areaOfFocus ? `My areas of focus are ${areaOfFocus}` : '')
-						.run();
-				} catch (error) {
-					console.log('Error saving goal:', error);
-				}
-
-				controller.close();
-			},
-		});
+		const stream = await createGoal(env, goal, areaOfFocus, timeline, user);
 
 		return new Response(stream, {
 			headers: {
--- src/utils/ai_completions.ts
+++ src/utils/ai_completions.ts
@@ -1,6 +1,84 @@
 import OpenAI from 'openai';
 import { Env } from '../types';
 
+export const createGoal = async (env: Env, goal: any, areaOfFocus: any, timeline: any, user: any) => {
+	const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY });
+	const completion = await openai.chat.completions.create({
+		stream: true,
+		messages: [
+			{
+				role: 'system',
+				content: `You are an expert in the field of ${goal}`,
+			},
+			{ role: 'user', content: `My goal is to ${goal}. ${areaOfFocus ? `My areas of focus are ${areaOfFocus}` : ''}` },
+			{
+				role: 'system',
+				content: `You are passionate about explaining sub points of the goal in detail`,
+			},
+			{ role: 'system', content: `Outline detailed plan to achieve the goal in ${timeline}.` },
+			{ role: 'system', content: `Expand or contract the plan relative to the length of the goal's timeline.` },
+			{
+				role: 'system',
+				content: `Please format your response in valid Markdown, adhering to the following:
+				- Use headings with "#" for levels (e.g., "#", "##").
+				- Do not use bullet points or numbered lists.
+				Ensure the Markdown is clean and easy to copy into any Markdown editor.`,
+			},
+		],
+		model: 'gpt-4o-mini',
+	});
+
+	let buffer = '';
+	let rawTotalResponse = '';
+	try {
+		const stream = new ReadableStream({
+			async start(controller) {
+				const encoder = new TextEncoder();
+				for await (const chunk of completion) {
+					let content = chunk.choices[0]?.delta?.content;
+
+					if (content) {
+						rawTotalResponse += content;
+						buffer += content;
+
+						let lines = buffer.split(/(?=\n|^#{1,4}|\s-\s|\n\s\*\s|\n\d+\.\s)/);
+						buffer = '';
+
+						lines.forEach((line, index) => {
+							if (index === lines.length - 1 && !line.endsWith('\n')) {
+								buffer = line;
+							} else {
+								controller.enqueue(encoder.encode(line));
+							}
+						});
+					}
+				}
+
+				if (buffer) {
+					controller.enqueue(encoder.encode(buffer));
+				}
+
+				controller.enqueue(encoder.encode(`event: done\n\n`));
+
+				try {
+					await env.DB.prepare(`UPDATE Users SET analyze_requests = analyze_requests - 1 WHERE user_id = ?`).bind(user.user_id).run();
+					await env.DB.prepare(`INSERT INTO Goals (user_id, goal_name, plan, time_line, aof) VALUES (?, ?, ?, ?, ?)`)
+						.bind(user.user_id, goal, rawTotalResponse, timeline, areaOfFocus ? `My areas of focus are ${areaOfFocus}` : '')
+						.run();
+				} catch (error) {
+					console.log('Error saving goal:', error);
+				}
+
+				controller.close();
+			},
+		});
+		return stream;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+};
+
 export const createSubGoal = async (env: Env, goal: any, sub_goal_name: string, line_number: number) => {
 	const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY });
 
