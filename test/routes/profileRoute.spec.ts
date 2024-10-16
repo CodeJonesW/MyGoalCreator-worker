01072779473b833d6ef29cfb26fa0c9b17f2e210
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-16 09:24:52 -0500

    add trackedGoal to returned data in profile route, update tests

--- test/routes/profileRoute.spec.ts
+++ test/routes/profileRoute.spec.ts
@@ -53,12 +53,12 @@ describe('Profile Route', () => {
 
 		// Mock user data and goals from the database
 		// @ts-ignore
-		mockEnv.DB.first.mockResolvedValue({
+		mockEnv.DB.first.mockResolvedValueOnce({
 			email: 'test@example.com',
 			analyze_requests: 10,
 		});
 		// @ts-ignore
-		mockEnv.DB.all.mockResolvedValue({
+		mockEnv.DB.all.mockResolvedValueOnce({
 			results: [{ goal_name: 'Learn React', goal_id: 1 }],
 		});
 
@@ -70,6 +70,59 @@ describe('Profile Route', () => {
 		expect(result.user.email).toBe('test@example.com');
 		// @ts-ignore
 		expect(result.goals[0].goal_name).toBe('Learn React');
+		// @ts-ignore
+		expect(result.trackedGoal).toBeNull();
+	});
+
+	it('should return 200 and user data to include tracked goal', async () => {
+		const request = new Request('http://localhost/api/profile', { method: 'GET' });
+
+		// Access the mocked verifyToken and mock its resolved value
+		const { verifyToken } = await import('../../src/utils/auth'); // Updated path to auth
+		// @ts-ignore
+		verifyToken.mockResolvedValue({
+			user: { user_id: 1, email: 'test@example.com' },
+		});
+
+		// Mock user query
+		// @ts-ignore
+		mockEnv.DB.first.mockResolvedValueOnce({
+			email: 'test@example.com',
+			analyze_requests: 10,
+		});
+		// mock all goals query
+		// @ts-ignore
+		mockEnv.DB.all.mockResolvedValueOnce({
+			results: [{ goal_name: 'Learn React', goal_id: 1 }],
+		});
+		// mock recent goal query
+		// @ts-ignore
+		mockEnv.DB.first.mockResolvedValueOnce({
+			goal_id: 1,
+			plan: 'Learn React',
+			aof: 'React is a JavaScript library for building user interfaces',
+			timeline: '1 month',
+		});
+
+		// mock tracked goal query
+		// @ts-ignore
+		mockEnv.DB.first.mockResolvedValueOnce({
+			goal_id: 1,
+			user_id: 1,
+		});
+
+		const response = await profileRoute(request, mockEnv);
+		const result = await response.json();
+
+		expect(response.status).toBe(200);
+		// @ts-ignore
+		expect(result.user.email).toBe('test@example.com');
+		// @ts-ignore
+		expect(result.goals[0].goal_name).toBe('Learn React');
+		// @ts-ignore
+		expect(result.trackedGoal.goal_id).toBe(1);
+		// @ts-ignore
+		expect(result.trackedGoal.user_id).toBe(1);
 	});
 
 	it('should return a 403 error if the token is invalid', async () => {
