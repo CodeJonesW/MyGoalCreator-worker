2724e13cd8d59bdbccb20a4fa13e7ce393673e94
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-18 00:17:59 -0500

    add new types goal and subgoal

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,20 @@ export interface User {
 	user_password?: string;
 	analyze_requests?: number;
 }
+
+export type Goal = {
+	goal_id: number;
+	goal_name: string;
+	plan: string;
+	time_line: string;
+	aof: string;
+	subgoals: SubGoal[];
+};
+
+export type SubGoal = {
+	sub_goal_id: number;
+	goal_id: number;
+	sub_goal_name: string;
+	line_number?: number;
+	sub_goal_plan?: string;
+};
