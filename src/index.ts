af3ebb57107dd9edf5584d021f9d15abe819d7f6
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-30 14:07:06 -0500

    update stream goal routes to match patterns

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,12 @@ import { Hono } from 'hono';
 const app = new Hono<{ Bindings: Env }>();
 
 app.post('/api/createGoal', createGoalRoute);
-app.post('/api/analyze', streamGoalRoute);
+app.post('/api/streamGoal', streamGoalRoute);
 app.get('/api/goal', goalByIdRoute);
 app.delete('/api/goal', deleteGoalByIdRoute);
 
 app.post('/api/createSubGoal', createSubGoalRoute);
-app.post('/api/subgoal', streamSubGoalRoute);
+app.post('/api/streamSubGoal', streamSubGoalRoute);
 
 app.post('/api/login', loginRoute);
 app.get('/api/profile', profileRoute);
