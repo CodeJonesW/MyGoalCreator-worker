242601d9257ed3cbca90348ac0889118852b4f7a
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-11-20 13:40:50 -0600

    add dayjs and use to parse user datetime for completed day in profile and complete day routes

--- src/routes/todo/completeDayRoute.ts
+++ src/routes/todo/completeDayRoute.ts
@@ -1,24 +1,41 @@
 import { Context } from 'hono';
 import { errorResponse } from '../../utils/response_utils';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export const completeDayRoute = async (context: Context): Promise<Response> => {
 	const { verifyToken } = await import('../../utils/auth');
 	try {
 		const { req: request, env } = context;
 		const authResponse = await verifyToken(request.raw, env);
 		if (authResponse instanceof Response) return authResponse;
+
+		const req_datetime = request.header('datetime');
+		const req_timezone = request.header('timezone');
+
 		const user = authResponse.user;
-		const today = new Date().toISOString().split('T')[0];
-		const result = await env.DB.prepare(
+
+		const dailyTodosCompletions = await env.DB.prepare(
 			`
-            SELECT *
-            FROM DailyTodoCompletions
-            WHERE user_id = ?
-              AND DATE(completed_at) = DATE(?)
-        `
+			SELECT *
+			FROM DailyTodoCompletions
+			WHERE user_id = ?
+			`
 		)
-			.bind(user.user_id, today)
-			.first();
-		if (result) {
+			.bind(user.user_id)
+			.all();
+
+		const completedToday = dailyTodosCompletions.results.some((completion: any) => {
+			const completionDate = dayjs(completion.completed_at).tz(req_timezone).format('YYYY-MM-DD');
+			const user_datetime = dayjs(req_datetime).format('YYYY-MM-DD');
+			return completionDate === user_datetime;
+		});
+
+		if (completedToday) {
 			return errorResponse('Todo already completed today', 403);
 		}
 		await env.DB.prepare(`INSERT INTO DailyTodoCompletions (user_id) VALUES (?)`).bind(user.user_id).run();
